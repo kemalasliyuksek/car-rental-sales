ca5c33fb97fc57dbc5541437f4018602f3d4d1a1
Kemal Aslıyüksek <kemalasliyuksek@icloud.com>
2025-04-22 19:10:21 +0300

    Update MainPage.cs

--- car-rental-sales-desktop/Forms/Pages/MainPage.cs
+++ car-rental-sales-desktop/Forms/Pages/MainPage.cs
@@ -17,11 +17,19 @@ namespace car_rental_sales_desktop.Forms.Pages
             InitializeComponent();
         }
 
+        private void MainPage_Load(object sender, EventArgs e)
+        {
+            lblUsername.Text = Utils.CurrentUser.FullName;
+            lblUserRole.Text = Utils.CurrentUser.RoleName;
+            lblBranchName.Text = Utils.CurrentUser.BranchName;
+        }
+
         private void btnLogOut_Click(object sender, EventArgs e)
         {
             var result = MessageBox.Show("Are you sure you want to log out?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
             if (result == DialogResult.Yes)
             {
+                Utils.CurrentUser.Logout();
                 this.Close();
                 LoginPage loginPage = new LoginPage();
                 loginPage.Show();
