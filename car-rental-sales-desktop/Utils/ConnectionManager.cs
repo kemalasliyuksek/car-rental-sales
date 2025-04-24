69760f86138334463aa5ab92462c8fe54d8864f0
Kemal Aslıyüksek <kemalasliyuksek@icloud.com>
2025-04-24 03:43:53 +0300

    Update ConnectionManager.cs

--- car-rental-sales-desktop/Utils/ConnectionManager.cs
+++ car-rental-sales-desktop/Utils/ConnectionManager.cs
@@ -1,71 +1,30 @@
 ﻿using System;
 using System.Configuration;
-using System.Security.Cryptography;
-using System.Text;
-using System.IO;
 using MySql.Data.MySqlClient;
 
 namespace car_rental_sales_desktop.Utils
 {
     public static class ConnectionManager
     {
-        private static readonly byte[] EncryptionKey = Encoding.UTF8.GetBytes("Arac123456789101");
-        private static readonly byte[] IV = Encoding.UTF8.GetBytes("1234567890123456");
-
         public static bool UseRemoteDatabase { get; set; } = false;
 
         public static string GetConnectionString()
         {
+            // Direkt App.config'ten bağlantı dizesini al
             string connectionName = UseRemoteDatabase ? "AracDB_Remote" : "AracDB_Local";
-            string encryptedConnectionString = ConfigurationManager.AppSettings[$"Encrypted_{connectionName}"];
 
-            if (string.IsNullOrEmpty(encryptedConnectionString))
-                throw new InvalidOperationException($"Şifreli bağlantı cümlesi bulunamadı: {connectionName}");
+            // ConnectionStrings bölümünden doğrudan al
+            ConnectionStringSettings connectionSettings = ConfigurationManager.ConnectionStrings[connectionName];
 
-            return DecryptConnectionString(encryptedConnectionString);
+            if (connectionSettings == null || string.IsNullOrEmpty(connectionSettings.ConnectionString))
+                throw new InvalidOperationException($"Bağlantı cümlesi bulunamadı: {connectionName}");
+
+            return connectionSettings.ConnectionString;
         }
 
         public static MySqlConnection GetConnection()
         {
             return new MySqlConnection(GetConnectionString());
-        }
-
-        public static string EncryptConnectionString(string connectionString)
-        {
-            using (Aes aes = Aes.Create())
-            {
-                aes.Key = EncryptionKey;
-                aes.IV = IV;
-
-                using (var encryptor = aes.CreateEncryptor())
-                using (var ms = new MemoryStream())
-                using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
-                using (var sw = new StreamWriter(cs))
-                {
-                    sw.Write(connectionString);
-                    sw.Close();
-                    return Convert.ToBase64String(ms.ToArray());
-                }
-            }
-        }
-
-        private static string DecryptConnectionString(string encrypted)
-        {
-            byte[] cipherText = Convert.FromBase64String(encrypted);
-
-            using (Aes aes = Aes.Create())
-            {
-                aes.Key = EncryptionKey;
-                aes.IV = IV;
-
-                using (var decryptor = aes.CreateDecryptor())
-                using (var ms = new MemoryStream(cipherText))
-                using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
-                using (var sr = new StreamReader(cs))
-                {
-                    return sr.ReadToEnd();
-                }
-            }
         }
     }
 }
