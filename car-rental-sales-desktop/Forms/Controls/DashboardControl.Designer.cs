541fc1ff5cd999fcc9f0ba0cfb4f82b27613f9f0
Kemal Aslıyüksek <kemalasliyuksek@icloud.com>
2025-04-26 01:10:30 +0300

    update

--- car-rental-sales-desktop/Forms/Controls/DashboardControl.Designer.cs
+++ car-rental-sales-desktop/Forms/Controls/DashboardControl.Designer.cs
@@ -29,12 +29,37 @@
         private void InitializeComponent()
         {
             pnlCards = new Panel();
-            pnlRentCard1 = new Panel();
-            pnlRentCard2 = new Panel();
-            pnlRentCard3 = new Panel();
-            pnlRentCard4 = new Panel();
             pnlRentCard5 = new Panel();
+            lblCanceledRentalsTitle = new Label();
+            lblCanceledRentalsCount = new Label();
+            iconCanceledRentals = new FontAwesome.Sharp.IconPictureBox();
+            pnlRentCard4 = new Panel();
+            lblMonthlyRentalsTitle = new Label();
+            lblMonthlyRentalsCount = new Label();
+            iconMonthlyRentals = new FontAwesome.Sharp.IconPictureBox();
+            pnlRentCard3 = new Panel();
+            lblActiveRentalsTitle = new Label();
+            lblActiveRentalsCount = new Label();
+            iconActiveRentals = new FontAwesome.Sharp.IconPictureBox();
+            pnlRentCard2 = new Panel();
+            lblTotalSalesTitle = new Label();
+            lblTotalSalesCount = new Label();
+            iconTotalSales = new FontAwesome.Sharp.IconPictureBox();
+            pnlRentCard1 = new Panel();
+            lblTotalRentalsTitle = new Label();
+            lblTotalRentalsCount = new Label();
+            iconTotalRentals = new FontAwesome.Sharp.IconPictureBox();
             pnlCards.SuspendLayout();
+            pnlRentCard5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)iconCanceledRentals).BeginInit();
+            pnlRentCard4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)iconMonthlyRentals).BeginInit();
+            pnlRentCard3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)iconActiveRentals).BeginInit();
+            pnlRentCard2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)iconTotalSales).BeginInit();
+            pnlRentCard1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)iconTotalRentals).BeginInit();
             SuspendLayout();
             // 
             // pnlCards
@@ -50,45 +75,250 @@
             pnlCards.Size = new Size(1570, 300);
             pnlCards.TabIndex = 0;
             // 
-            // pnlRentCard1
+            // pnlRentCard5
             // 
-            pnlRentCard1.Dock = DockStyle.Left;
-            pnlRentCard1.Location = new Point(0, 0);
-            pnlRentCard1.Name = "pnlRentCard1";
-            pnlRentCard1.Size = new Size(314, 300);
-            pnlRentCard1.TabIndex = 0;
+            pnlRentCard5.BackColor = Color.FromArgb(255, 158, 158);
+            pnlRentCard5.Controls.Add(lblCanceledRentalsTitle);
+            pnlRentCard5.Controls.Add(lblCanceledRentalsCount);
+            pnlRentCard5.Controls.Add(iconCanceledRentals);
+            pnlRentCard5.Dock = DockStyle.Left;
+            pnlRentCard5.Location = new Point(1256, 0);
+            pnlRentCard5.Margin = new Padding(10);
+            pnlRentCard5.Name = "pnlRentCard5";
+            pnlRentCard5.Size = new Size(314, 300);
+            pnlRentCard5.TabIndex = 4;
             // 
-            // pnlRentCard2
+            // lblCanceledRentalsTitle
             // 
-            pnlRentCard2.Dock = DockStyle.Left;
-            pnlRentCard2.Location = new Point(314, 0);
-            pnlRentCard2.Name = "pnlRentCard2";
-            pnlRentCard2.Size = new Size(314, 300);
-            pnlRentCard2.TabIndex = 1;
+            lblCanceledRentalsTitle.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblCanceledRentalsTitle.ForeColor = Color.FromArgb(50, 50, 50);
+            lblCanceledRentalsTitle.Location = new Point(20, 200);
+            lblCanceledRentalsTitle.Name = "lblCanceledRentalsTitle";
+            lblCanceledRentalsTitle.Size = new Size(274, 50);
+            lblCanceledRentalsTitle.TabIndex = 2;
+            lblCanceledRentalsTitle.Text = "Canceled Rentals";
+            lblCanceledRentalsTitle.TextAlign = ContentAlignment.MiddleCenter;
             // 
-            // pnlRentCard3
+            // lblCanceledRentalsCount
             // 
-            pnlRentCard3.Dock = DockStyle.Left;
-            pnlRentCard3.Location = new Point(628, 0);
-            pnlRentCard3.Name = "pnlRentCard3";
-            pnlRentCard3.Size = new Size(314, 300);
-            pnlRentCard3.TabIndex = 2;
+            lblCanceledRentalsCount.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            lblCanceledRentalsCount.ForeColor = Color.FromArgb(50, 50, 50);
+            lblCanceledRentalsCount.Location = new Point(20, 120);
+            lblCanceledRentalsCount.Name = "lblCanceledRentalsCount";
+            lblCanceledRentalsCount.Size = new Size(274, 70);
+            lblCanceledRentalsCount.TabIndex = 1;
+            lblCanceledRentalsCount.Text = "0";
+            lblCanceledRentalsCount.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // iconCanceledRentals
+            // 
+            iconCanceledRentals.BackColor = Color.Transparent;
+            iconCanceledRentals.ForeColor = Color.FromArgb(80, 80, 80);
+            iconCanceledRentals.IconChar = FontAwesome.Sharp.IconChar.CalendarXmark;
+            iconCanceledRentals.IconColor = Color.FromArgb(80, 80, 80);
+            iconCanceledRentals.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconCanceledRentals.IconSize = 60;
+            iconCanceledRentals.Location = new Point(127, 40);
+            iconCanceledRentals.Name = "iconCanceledRentals";
+            iconCanceledRentals.Size = new Size(60, 60);
+            iconCanceledRentals.TabIndex = 0;
+            iconCanceledRentals.TabStop = false;
             // 
             // pnlRentCard4
             // 
+            pnlRentCard4.BackColor = Color.FromArgb(255, 213, 128);
+            pnlRentCard4.Controls.Add(lblMonthlyRentalsTitle);
+            pnlRentCard4.Controls.Add(lblMonthlyRentalsCount);
+            pnlRentCard4.Controls.Add(iconMonthlyRentals);
             pnlRentCard4.Dock = DockStyle.Left;
             pnlRentCard4.Location = new Point(942, 0);
+            pnlRentCard4.Margin = new Padding(10);
             pnlRentCard4.Name = "pnlRentCard4";
             pnlRentCard4.Size = new Size(314, 300);
             pnlRentCard4.TabIndex = 3;
             // 
-            // pnlRentCard5
+            // lblMonthlyRentalsTitle
             // 
-            pnlRentCard5.Dock = DockStyle.Left;
-            pnlRentCard5.Location = new Point(1256, 0);
-            pnlRentCard5.Name = "pnlRentCard5";
-            pnlRentCard5.Size = new Size(314, 300);
-            pnlRentCard5.TabIndex = 4;
+            lblMonthlyRentalsTitle.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblMonthlyRentalsTitle.ForeColor = Color.FromArgb(50, 50, 50);
+            lblMonthlyRentalsTitle.Location = new Point(20, 200);
+            lblMonthlyRentalsTitle.Name = "lblMonthlyRentalsTitle";
+            lblMonthlyRentalsTitle.Size = new Size(274, 50);
+            lblMonthlyRentalsTitle.TabIndex = 2;
+            lblMonthlyRentalsTitle.Text = "Monthly Rentals (Last 30 Days)";
+            lblMonthlyRentalsTitle.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblMonthlyRentalsCount
+            // 
+            lblMonthlyRentalsCount.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            lblMonthlyRentalsCount.ForeColor = Color.FromArgb(50, 50, 50);
+            lblMonthlyRentalsCount.Location = new Point(20, 120);
+            lblMonthlyRentalsCount.Name = "lblMonthlyRentalsCount";
+            lblMonthlyRentalsCount.Size = new Size(274, 70);
+            lblMonthlyRentalsCount.TabIndex = 1;
+            lblMonthlyRentalsCount.Text = "0";
+            lblMonthlyRentalsCount.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // iconMonthlyRentals
+            // 
+            iconMonthlyRentals.BackColor = Color.Transparent;
+            iconMonthlyRentals.ForeColor = Color.FromArgb(80, 80, 80);
+            iconMonthlyRentals.IconChar = FontAwesome.Sharp.IconChar.CalendarDay;
+            iconMonthlyRentals.IconColor = Color.FromArgb(80, 80, 80);
+            iconMonthlyRentals.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconMonthlyRentals.IconSize = 60;
+            iconMonthlyRentals.Location = new Point(127, 40);
+            iconMonthlyRentals.Name = "iconMonthlyRentals";
+            iconMonthlyRentals.Size = new Size(60, 60);
+            iconMonthlyRentals.TabIndex = 0;
+            iconMonthlyRentals.TabStop = false;
+            // 
+            // pnlRentCard3
+            // 
+            pnlRentCard3.BackColor = Color.FromArgb(135, 206, 250);
+            pnlRentCard3.Controls.Add(lblActiveRentalsTitle);
+            pnlRentCard3.Controls.Add(lblActiveRentalsCount);
+            pnlRentCard3.Controls.Add(iconActiveRentals);
+            pnlRentCard3.Dock = DockStyle.Left;
+            pnlRentCard3.Location = new Point(628, 0);
+            pnlRentCard3.Margin = new Padding(10);
+            pnlRentCard3.Name = "pnlRentCard3";
+            pnlRentCard3.Size = new Size(314, 300);
+            pnlRentCard3.TabIndex = 2;
+            // 
+            // lblActiveRentalsTitle
+            // 
+            lblActiveRentalsTitle.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblActiveRentalsTitle.ForeColor = Color.FromArgb(50, 50, 50);
+            lblActiveRentalsTitle.Location = new Point(20, 200);
+            lblActiveRentalsTitle.Name = "lblActiveRentalsTitle";
+            lblActiveRentalsTitle.Size = new Size(274, 50);
+            lblActiveRentalsTitle.TabIndex = 2;
+            lblActiveRentalsTitle.Text = "Active Rentals";
+            lblActiveRentalsTitle.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblActiveRentalsCount
+            // 
+            lblActiveRentalsCount.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            lblActiveRentalsCount.ForeColor = Color.FromArgb(50, 50, 50);
+            lblActiveRentalsCount.Location = new Point(20, 120);
+            lblActiveRentalsCount.Name = "lblActiveRentalsCount";
+            lblActiveRentalsCount.Size = new Size(274, 70);
+            lblActiveRentalsCount.TabIndex = 1;
+            lblActiveRentalsCount.Text = "0";
+            lblActiveRentalsCount.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // iconActiveRentals
+            // 
+            iconActiveRentals.BackColor = Color.Transparent;
+            iconActiveRentals.ForeColor = Color.FromArgb(80, 80, 80);
+            iconActiveRentals.IconChar = FontAwesome.Sharp.IconChar.CarSide;
+            iconActiveRentals.IconColor = Color.FromArgb(80, 80, 80);
+            iconActiveRentals.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconActiveRentals.IconSize = 60;
+            iconActiveRentals.Location = new Point(127, 40);
+            iconActiveRentals.Name = "iconActiveRentals";
+            iconActiveRentals.Size = new Size(60, 60);
+            iconActiveRentals.TabIndex = 0;
+            iconActiveRentals.TabStop = false;
+            // 
+            // pnlRentCard2
+            // 
+            pnlRentCard2.BackColor = Color.FromArgb(144, 238, 144);
+            pnlRentCard2.Controls.Add(lblTotalSalesTitle);
+            pnlRentCard2.Controls.Add(lblTotalSalesCount);
+            pnlRentCard2.Controls.Add(iconTotalSales);
+            pnlRentCard2.Dock = DockStyle.Left;
+            pnlRentCard2.Location = new Point(314, 0);
+            pnlRentCard2.Margin = new Padding(10);
+            pnlRentCard2.Name = "pnlRentCard2";
+            pnlRentCard2.Size = new Size(314, 300);
+            pnlRentCard2.TabIndex = 1;
+            // 
+            // lblTotalSalesTitle
+            // 
+            lblTotalSalesTitle.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblTotalSalesTitle.ForeColor = Color.FromArgb(50, 50, 50);
+            lblTotalSalesTitle.Location = new Point(20, 200);
+            lblTotalSalesTitle.Name = "lblTotalSalesTitle";
+            lblTotalSalesTitle.Size = new Size(274, 50);
+            lblTotalSalesTitle.TabIndex = 2;
+            lblTotalSalesTitle.Text = "Total Sales";
+            lblTotalSalesTitle.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblTotalSalesCount
+            // 
+            lblTotalSalesCount.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            lblTotalSalesCount.ForeColor = Color.FromArgb(50, 50, 50);
+            lblTotalSalesCount.Location = new Point(20, 120);
+            lblTotalSalesCount.Name = "lblTotalSalesCount";
+            lblTotalSalesCount.Size = new Size(274, 70);
+            lblTotalSalesCount.TabIndex = 1;
+            lblTotalSalesCount.Text = "0";
+            lblTotalSalesCount.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // iconTotalSales
+            // 
+            iconTotalSales.BackColor = Color.Transparent;
+            iconTotalSales.ForeColor = Color.FromArgb(80, 80, 80);
+            iconTotalSales.IconChar = FontAwesome.Sharp.IconChar.Tags;
+            iconTotalSales.IconColor = Color.FromArgb(80, 80, 80);
+            iconTotalSales.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconTotalSales.IconSize = 60;
+            iconTotalSales.Location = new Point(127, 40);
+            iconTotalSales.Name = "iconTotalSales";
+            iconTotalSales.Size = new Size(60, 60);
+            iconTotalSales.TabIndex = 0;
+            iconTotalSales.TabStop = false;
+            // 
+            // pnlRentCard1
+            // 
+            pnlRentCard1.BackColor = Color.FromArgb(171, 197, 255);
+            pnlRentCard1.Controls.Add(lblTotalRentalsTitle);
+            pnlRentCard1.Controls.Add(lblTotalRentalsCount);
+            pnlRentCard1.Controls.Add(iconTotalRentals);
+            pnlRentCard1.Dock = DockStyle.Left;
+            pnlRentCard1.Location = new Point(0, 0);
+            pnlRentCard1.Margin = new Padding(10);
+            pnlRentCard1.Name = "pnlRentCard1";
+            pnlRentCard1.Size = new Size(314, 300);
+            pnlRentCard1.TabIndex = 0;
+            // 
+            // lblTotalRentalsTitle
+            // 
+            lblTotalRentalsTitle.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblTotalRentalsTitle.ForeColor = Color.FromArgb(50, 50, 50);
+            lblTotalRentalsTitle.Location = new Point(20, 200);
+            lblTotalRentalsTitle.Name = "lblTotalRentalsTitle";
+            lblTotalRentalsTitle.Size = new Size(274, 50);
+            lblTotalRentalsTitle.TabIndex = 2;
+            lblTotalRentalsTitle.Text = "Total Rentals";
+            lblTotalRentalsTitle.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblTotalRentalsCount
+            // 
+            lblTotalRentalsCount.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            lblTotalRentalsCount.ForeColor = Color.FromArgb(50, 50, 50);
+            lblTotalRentalsCount.Location = new Point(20, 120);
+            lblTotalRentalsCount.Name = "lblTotalRentalsCount";
+            lblTotalRentalsCount.Size = new Size(274, 70);
+            lblTotalRentalsCount.TabIndex = 1;
+            lblTotalRentalsCount.Text = "0";
+            lblTotalRentalsCount.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // iconTotalRentals
+            // 
+            iconTotalRentals.BackColor = Color.Transparent;
+            iconTotalRentals.ForeColor = Color.FromArgb(80, 80, 80);
+            iconTotalRentals.IconChar = FontAwesome.Sharp.IconChar.CalendarCheck;
+            iconTotalRentals.IconColor = Color.FromArgb(80, 80, 80);
+            iconTotalRentals.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconTotalRentals.IconSize = 60;
+            iconTotalRentals.Location = new Point(127, 40);
+            iconTotalRentals.Name = "iconTotalRentals";
+            iconTotalRentals.Size = new Size(60, 60);
+            iconTotalRentals.TabIndex = 0;
+            iconTotalRentals.TabStop = false;
             // 
             // DashboardControl
             // 
@@ -98,7 +328,18 @@
             Name = "DashboardControl";
             Padding = new Padding(50);
             Size = new Size(1670, 1000);
+            Load += DashboardControl_Load;
             pnlCards.ResumeLayout(false);
+            pnlRentCard5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)iconCanceledRentals).EndInit();
+            pnlRentCard4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)iconMonthlyRentals).EndInit();
+            pnlRentCard3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)iconActiveRentals).EndInit();
+            pnlRentCard2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)iconTotalSales).EndInit();
+            pnlRentCard1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)iconTotalRentals).EndInit();
             ResumeLayout(false);
         }
 
@@ -110,5 +351,25 @@
         private Panel pnlRentCard3;
         private Panel pnlRentCard2;
         private Panel pnlRentCard1;
+        private FontAwesome.Sharp.IconPictureBox iconTotalRentals;
+        private Label lblTotalRentalsCount;
+        private Label lblTotalRentalsTitle;
+        private Label lblTotalSalesTitle;
+        private Label lblTotalSalesCount;
+        private FontAwesome.Sharp.IconPictureBox iconTotalSales;
+        private Label lblActiveRentalsTitle;
+        private Label lblActiveRentalsCount;
+        private FontAwesome.Sharp.IconPictureBox iconActiveRentals;
+        private Label lblMonthlyRentalsTitle;
+        private Label lblMonthlyRentalsCount;
+        private FontAwesome.Sharp.IconPictureBox iconMonthlyRentals;
+        private Label lblCanceledRentalsTitle;
+        private Label lblCanceledRentalsCount;
+        private FontAwesome.Sharp.IconPictureBox iconCanceledRentals;
+        private Panel pnlCharts;
+        private Panel pnlVehicleChart;
+        private Label lblVehicleChartTitle;
+        private Panel pnlRevenueChart;
+        private Label lblRevenueChartTitle;
     }
 }
--- car-rental-sales-desktop/Forms/Controls/DashboardControl.cs
+++ car-rental-sales-desktop/Forms/Controls/DashboardControl.cs
@@ -1,20 +1,93 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using car_rental_sales_desktop.Repositories;
+using System;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace car_rental_sales_desktop.Forms.Controls
 {
     public partial class DashboardControl : UserControl
     {
+        private ReportRepository _reportRepository;
+        private RentalRepository _rentalRepository;
+        private SaleRepository _saleRepository;
+        private VehicleRepository _vehicleRepository;
+
         public DashboardControl()
         {
             InitializeComponent();
+
+            // Initialize repositories
+            _reportRepository = new ReportRepository();
+            _rentalRepository = new RentalRepository();
+            _saleRepository = new SaleRepository();
+            _vehicleRepository = new VehicleRepository();
+        }
+
+        private void DashboardControl_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                LoadStatistics();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"İstatistik verileri yüklenirken hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LoadStatistics()
+        {
+            // Get dashboard summary data
+            DataTable dashboardData = _reportRepository.GetDashboardSummary();
+            if (dashboardData != null && dashboardData.Rows.Count > 0)
+            {
+                DataRow row = dashboardData.Rows[0];
+
+                // 3. Active Rentals
+                lblActiveRentalsCount.Text = row["ActiveRentals"].ToString();
+
+                // 4. Recent Sales (Last 30 days)
+                lblMonthlyRentalsCount.Text = row["RecentSales"].ToString();
+
+                // 5. Overdue Rentals (Using this as Canceled Rentals for now)
+                lblCanceledRentalsCount.Text = row["OverdueRentals"].ToString();
+            }
+
+            // Get total rentals count
+            int totalRentalsCount = GetTotalRentalsCount();
+            lblTotalRentalsCount.Text = totalRentalsCount.ToString();
+
+            // Get total sales count
+            int totalSalesCount = GetTotalSalesCount();
+            lblTotalSalesCount.Text = totalSalesCount.ToString();
+        }
+
+        private int GetTotalRentalsCount()
+        {
+            try
+            {
+                string query = "SELECT COUNT(*) FROM Rentals";
+                object result = Utils.DatabaseHelper.ExecuteScalar(query);
+                return Convert.ToInt32(result);
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        private int GetTotalSalesCount()
+        {
+            try
+            {
+                string query = "SELECT COUNT(*) FROM Sales";
+                object result = Utils.DatabaseHelper.ExecuteScalar(query);
+                return Convert.ToInt32(result);
+            }
+            catch
+            {
+                return 0;
+            }
         }
     }
 }
